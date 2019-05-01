41d55affff1580d1ad80a6845d04c813e2cca893
Zina <doan.zina@gmail.com>
2019-05-01 13:21:23 -0500

    Update serializers.py

--- modules/kneed_help/models/serializers.py
+++ modules/kneed_help/models/serializers.py
@@ -1,6 +1,7 @@
 from modules.extensions import ma
 from .body import PartOfBodyModel
 from .subparts import SubpartsModel
+from .conditions import ConditionsModel
 from marshmallow_sqlalchemy import ModelSchema
 
 
@@ -11,7 +12,12 @@ class BodyPartSchema(ModelSchema):
 class BodySubpartSchema(ModelSchema):
     class Meta:
         model = SubpartsModel
+        
+class ConditionsSchema(ModelSchema): 
+    class Meta:
+        model = ConditionsModel
 
 
 body_parts_schema = BodyPartSchema(many=True)
 body_subparts_schema = BodySubpartSchema(many=True)
+condition_schema = ConditionsModel()
