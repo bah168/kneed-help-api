3467fba2299bd3653196de0b7b6e7c86661885f5
Zina <doan.zina@gmail.com>
2019-05-01 13:40:57 -0500

    Update controllers.py

--- modules/kneed_help/controllers.py
+++ modules/kneed_help/controllers.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource, reqparse
 from flask import jsonify
-from .models.serializers import body_parts_schema, body_subparts_schema
-from .models import PartOfBodyModel, SubpartsModel
+from .models.serializers import body_parts_schema, body_subparts_schema, condition_schema
+from .models import PartOfBodyModel, SubpartsModel, ConditionsModel
 
 
 class BodyPartsList(Resource):
@@ -24,4 +24,20 @@ class BodySubpartForBodyPart(Resource):
 
         subparts = SubpartsModel.query.filter(SubpartsModel.body_part_id == body_part_id).all()
         return jsonify(subparts=body_subparts_schema.dump(subparts).data)
+    
+
+class Condition(Resource):
+
+    def __init__(self):
+        parser = reqparse.RequestParser(bundle_errors=True)
+        parser.add_argument("id", type=int)
+        self.args = parser.parse_args()
+        super().__init__()
+
+    def get(self):
+        condition_id = self.args['id']
+
+        conditions = ConditionsModel.query.filter(ConditionsModel.condition_id == condition_id).first()
+        return jsonify(conditions=condition_schema.dump(conditions).data)
+    
 
